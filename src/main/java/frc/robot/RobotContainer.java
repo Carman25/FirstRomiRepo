9358f22c52145f9681ddeed721033908790fdb86
Carson Whitehouse <carson.whitehouse@gmail.com>
2021-02-25 20:38:01 -0500

    Changed percent of power each joystick controls.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -176,6 +176,6 @@ public class RobotContainer {
    */
   public Command getArcadeDriveCommand() {
     return new ArcadeDrive(
-        m_drivetrain, () -> -m_controller.getRawAxis(1), () -> m_controller.getRawAxis(2));
+        m_drivetrain, () -> 0.8 * -m_controller.getRawAxis(1), () -> 0.2 * m_controller.getRawAxis(2));
   }
 }
